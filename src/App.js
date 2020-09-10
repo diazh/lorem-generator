40b1c1c145cf26a14ac63ec9b6dfdae8fcf12d9a
hectord <hectord@fwscience.com>
2020-09-10 14:11:23 -0700

    add links in menu

--- src/App.js
+++ src/App.js
@@ -16,9 +16,8 @@ class App extends Component {
       <Layout className="layout" style={{height: "100vh"}}>
         <Header style={{textAlign: "right"}}>
           <Menu mode="horizontal" style={{width: "100%", textAlign: "right"}}>
-            <Menu.Item key="1">Lorem</Menu.Item>
-            <Menu.Item key="2">Github</Menu.Item>
-            <Menu.Item key="3">Estudio Media</Menu.Item>
+            <Menu.Item key="2"><a href="https://github.com/diazh/lorem-generator" target="_blank">Github</a></Menu.Item>
+            <Menu.Item key="3"><a href="http://estudiomediamx.com" target="_blank">Estudio Media</a></Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
