ddbbf293fb4ae786b43da1b0b27e09c40f3e1039
hectord <hectord@fwscience.com>
2020-09-17 08:06:23 -0700

    changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Layout, Menu, Breadcrumb } from 'antd';
 import Topbar from './containers/Topbar/Topbar';
 import MainApp from './containers/App/App';
 import 'antd/dist/antd.css';
+import { BrowserRouter } from 'react-router-dom';
 
 const { Header, Content, Footer } = Layout;
 class App extends Component {
@@ -13,18 +14,20 @@ class App extends Component {
 
 
     return (
-      <Layout className="layout" style={{height: "100vh"}}>
-        <Header style={{textAlign: "right"}}>
-          <Menu mode="horizontal" style={{width: "100%", textAlign: "right"}}>
+      <Layout className="layout" style={{ height: "100vh" }}>
+        <Header style={{ textAlign: "right" }}>
+          <Menu mode="horizontal" style={{ width: "100%", textAlign: "right" }}>
             <Menu.Item key="2"><a href="https://github.com/diazh/lorem-generator" target="_blank">Github</a></Menu.Item>
             <Menu.Item key="3"><a href="http://estudiomediamx.com" target="_blank">Estudio Media</a></Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
-          
-          <MainApp />
+          <BrowserRouter basename="/lorem/">
+            <MainApp />
+          </BrowserRouter>
+
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Héctor Díaz ©2020</Footer>
+        <Footer style={{ textAlign: 'center' }}>EstudioMediaMx ©2020</Footer>
       </Layout>
     )
   }
