73b7815dc667650a05cafc35af8adc45e094adf5
hectord <hectord@fwscience.com>
2020-09-22 15:34:42 -0700

    update general data

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import { Layout, Menu, Breadcrumb } from 'antd';
-import Topbar from './containers/Topbar/Topbar';
+import { Layout, Menu } from 'antd';
 import MainApp from './containers/App/App';
 import 'antd/dist/antd.css';
 import { BrowserRouter } from 'react-router-dom';
@@ -17,8 +15,8 @@ class App extends Component {
       <Layout className="layout" style={{ height: "100vh" }}>
         <Header style={{ textAlign: "right" }}>
           <Menu mode="horizontal" style={{ width: "100%", textAlign: "right" }}>
-            <Menu.Item key="2"><a href="https://github.com/diazh/lorem-generator" target="_blank">Github</a></Menu.Item>
-            <Menu.Item key="3"><a href="http://estudiomediamx.com" target="_blank">Estudio Media</a></Menu.Item>
+            <Menu.Item key="2"><a href="https://github.com/diazh/lorem-generator" target="_blank" rel="noopener noreferrer">Github</a></Menu.Item>
+            <Menu.Item key="3"><a href="http://estudiomediamx.com" target="_blank" rel="noopener noreferrer">Estudio Media</a></Menu.Item>
           </Menu>
         </Header>
         <Content style={{ padding: '0 50px' }}>
